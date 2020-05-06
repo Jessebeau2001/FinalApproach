025682976d6654125b090e2023f00c039459a0bb
Jessebeau2001 <jessegamen@gmail.com>
2020-05-06 15:21:32 +0200

    Added some Classes

--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -1,33 +1,23 @@
-using System;									// System contains a lot of default C# libraries 
-using GXPEngine;								// GXPEngine contains the engine
+using System;
+using GXPEngine;
 using System.Drawing;
 
 public class MyGame : Game
 {
-	public MyGame() : base(800, 600, false)		// Create a window that's 800x600 and NOT fullscreen
+	Stage stage;
+	public MyGame() : base(1920, 1080, false)
 	{
-        //----------------------------------------------------example-code----------------------------
-        //create a canvas
-        Canvas canvas = new Canvas(800, 600);
-
-        //add some content
-        canvas.graphics.FillRectangle(new SolidBrush(Color.Red), new Rectangle(0, 0, 400, 300));
-        canvas.graphics.FillRectangle(new SolidBrush(Color.Blue), new Rectangle(400, 0, 400, 300));
-        canvas.graphics.FillRectangle(new SolidBrush(Color.Yellow), new Rectangle(0, 300, 400, 300));
-        canvas.graphics.FillRectangle(new SolidBrush(Color.Gray), new Rectangle(400, 300, 400, 300));
-
-        //add canvas to display list
-        AddChild(canvas);
-        //------------------------------------------------end-of-example-code-------------------------
+		stage = new Stage();
+		AddChild(stage);
     }
 
     void Update()
 	{
-		// Empty
+		
 	}
 
-	static void Main()							// Main() is the first method that's called when the program is run
+	static void Main()
 	{
-		new MyGame().Start();					// Create a "MyGame" and start it
+		new MyGame().Start();
 	}
 }
---
+++ GXPEngine/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine.GXPEngine
+{
+	class Player : Sprite
+	{
+		public Player() : base("circle.png")
+		{
+
+		}
+
+		void Update()
+		{
+
+		}
+	}
+}
