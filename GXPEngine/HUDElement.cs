0a47283c5381f524ee167b8b02392533f8d7a9c0
FenexisHighland <64906860+FenexisHighland@users.noreply.github.com>
2020-05-11 11:27:48 +0200

    Working, working
    Progress is being made

--- GXPEngine/HUDElement.cs
+++ GXPEngine/HUDElement.cs
@@ -12,7 +12,23 @@ namespace GXPEngine
         Sprite _listObject; //For pictures to overlay over the HUD
         EasyDraw _listText; //For text to overlay over the HUD
 
-        public HUDElement(float x, float y) : base("")
+        public HUDElement(float x, float y, Sprite item) : base("")
+        {
+            this.x = x;
+            this.y = y;
+            _listObject = item;
+        }
+
+        public HUDElement(float x, float y, string text) : base("")
+        {
+            _listText.TextAlign(CenterMode.Min, CenterMode.Min);
+
+            _listText.Clear(0);
+
+            _listText.Text(text, x, y);
+        }
+
+        public void Update()
         {
 
         }
--- GXPEngine/HUDOverlay.cs
+++ GXPEngine/HUDOverlay.cs
@@ -7,9 +7,16 @@ namespace GXPEngine
 {
     class HUDOverlay : GameObject
     {
+        HUDElement _listObject;
+        HUDElement _listText;
+
         public HUDOverlay(Inventory InvRef) //Reference to where the items are read from
         {
+            _listObject = new HUDElement(0, 0, "/texture/Onion.png");
+            AddChild(_listObject);
 
+            _listText = new HUDElement(0, 0, "Onion");
+            AddChild(_listText);
         }
 
         public void Update() //Use to update the current state
