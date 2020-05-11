1a9b7fb38511b0a90aa499a1f398e09424740f0b
FenexisHighland <64906860+FenexisHighland@users.noreply.github.com>
2020-05-11 11:04:34 +0200

    Baseline Framewok for HUD

---
+++ GXPEngine/HUDElement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine
+{
+    class HUDElement : Sprite
+    {
+        float _width;       //Non class-bound width
+        float _height;      //and height
+        Sprite _listObject; //For pictures to overlay over the HUD
+        EasyDraw _listText; //For text to overlay over the HUD
+
+        public HUDElement(float x, float y) : base("")
+        {
+
+        }
+    }
+}
---
+++ GXPEngine/HUDOverlay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine
+{
+    class HUDOverlay : GameObject
+    {
+        public HUDOverlay(Inventory InvRef) //Reference to where the items are read from
+        {
+
+        }
+
+        public void Update() //Use to update the current state
+        {
+
+        }
+    }
+}
